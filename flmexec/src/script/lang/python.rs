42d6594258af8a53d456e455fe5e9153ceabc9c1
Klaus Ma <klausm@nvidia.com>
2026-05-26 08:32:29 +0800

    style: fix rustfmt formatting in python.rs

--- flmexec/src/script/lang/python.rs
+++ flmexec/src/script/lang/python.rs
@@ -124,12 +124,7 @@ impl ScriptEngine for PythonScript {
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .current_dir(&self.runtime.work_dir)
-            .args([
-                "run",
-                "--python",
-                &python_version,
-                &self.runtime.entrypoint,
-            ])
+            .args(["run", "--python", &python_version, &self.runtime.entrypoint])
             .envs(self.runtime.env.iter().map(|(k, v)| (k.clone(), v.clone())))
             .spawn()
             .map_err(|e| FlameError::Internal(format!("failed to start subprocess: {e}")))?;
